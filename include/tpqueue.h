d50e54377dfbec66d8b9f7ba38441c465f6f55ab
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-04-22 13:43:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,55 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T value;
+    Node* next;
+  };
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+  void push(T);
+  int pop();
 };
 
 struct SYM {
   char ch;
   int prior;
 };
+
+template<typename T>
+void TPQueue<T>::push(T val) {
+  Node* newNode = new Node;
+  newNode->next = nullptr;
+  newNode->value = val;
+  if (!head) {
+    head = newNode;
+    return;
+  }
+  if (val.prior > head->value.prior) {
+    newNode->next = head;
+    head = newNode;
+    return;
+  }
+  Node* temp = head;
+  while (temp->next && val.prior <= temp->next->value.prior) temp = temp->next;
+  newNode->next = temp->next;
+  temp->next = newNode;
+}
+
+template<typename T>
+int TPQueue<T>::pop() {
+  if (!head) {
+    cout << "Очередь пуста!" << endl;
+    return -1;
+  }
+  int data = head->value.ch;
+  Node* temp = head;
+  head = head->next;
+  delete temp;
+  return data;
+}
 
 #endif  // INCLUDE_TPQUEUE_H_
